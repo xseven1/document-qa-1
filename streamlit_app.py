4fc5445d40e1e3b668abcf5ba47b58e6839abacb
Udayan Gaikwad <ggwpfax@gmail.com>
2025-09-04 14:33:41 +0000

    .

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,8 @@
 import streamlit as st
-
-st.set_page_config(
-    page_title="My First Multi-Page Streamlit Application",
-    page_icon=":material/edit:",
-)
-
-# Title in the sidebar
-st.sidebar.title("💥 Multi-Page Navigation")
-
+st.title("💥Multi-Page Navigation")
 lab1 = st.Page("lab1.py", title="LAB 1", icon=":material/arrow_outward:")
 lab2 = st.Page("lab2.py", title="LAB 2", icon=":material/arrow_outward:")
 
 pg = st.navigation([lab1, lab2])
+st.set_page_config(page_title="My First Multi-Page Streamlit Application", page_icon=":material/edit:")
 pg.run()
